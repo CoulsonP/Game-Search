abbabe235b93da8079a8aff358a5ca366c18fc9a
kirchnerdev22 <trentonlkirchner@gmail.com>
2022-11-28 17:33:21 -0600

    modal-has-been-added

--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,95 @@
 const url = 'https://api.rawg.io/api/games?key=66a5bc708a104e548eceb91211ed4232&dates=2019-09-01,2019-09-30&platforms=18,1,7';
-
-
-
+const modalContent = document.getElementById('modalContent');
+let modal = $('.modal');
+let currentGame;
 
 fetch(url)
 
-	.then(res => res.json())
-	.then(data => {console.log(data)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
 
-var img1 = document.createElement("img")
-img1.setAttribute("src", data.results[0].background_image)
+    var img1 = document.createElement("img")
+    img1.setAttribute("src", data.results[0].background_image)
+    img1.click(function () {
+      currentGame = data.results[0];
+    });
 
-var img2 = document.createElement("img")
-img2.setAttribute("src", data.results[1].background_image)
+    var img2 = document.createElement("img")
+    img2.setAttribute("src", data.results[1].background_image)
+    img2.click(function () {
+      currentGame = data.results[1];
+    });
 
-var img3 = document.createElement("img")
-img3.setAttribute("src", data.results[2].background_image)
+    var img3 = document.createElement("img")
+    img3.setAttribute("src", data.results[2].background_image)
+    img3.click(function () {
+      currentGame = data.results[2];
+    });
 
-var img4 = document.createElement("img")
-img4.setAttribute("src", data.results[3].background_image)
-
-var img5 = document.createElement("img")
-img5.setAttribute("src", data.results[4].background_image)
-
-var img6 = document.createElement("img")
-img6.setAttribute("src", data.results[5].background_image)
-
-
+    var img4 = document.createElement("img")
+    img4.setAttribute("src", data.results[3].background_image)
+    img4.click(function () {
+      currentGame = data.results[3];
+    });
 
 
-// for (i = 0; i < data.results.length; i++){
-//     var carouselItem = $("<a>")
-//     carouselItem.addClass("carousel-item")
-//     carouselItem.attr("src", data.results[i].background_image)
-//     $(".carousel").append(carouselItem)
-// }
-
-document.querySelector(".item1").append(img1)
-document.querySelector(".item2").append(img2)
-document.querySelector(".item3").append(img3)
-document.querySelector(".item4").append(img4)
-    })
-	.catch(err => console.error('error:' + err));
-
+    document.querySelector(".item1").append(img1)
+    document.querySelector(".item2").append(img2)
+    document.querySelector(".item3").append(img3)
+    document.querySelector(".item4").append(img4)    
     
-    
-      // Or with jQuery
-    
-      $(document).ready(function(){
-        $('.carousel').carousel();
-      });
+    // for (i = 0; i < data.results.length; i++){
+    //     var carouselItem = $("<a>")
+    //     carouselItem.addClass("carousel-item")
+    //     carouselItem.attr("src", data.results[i].background_image)
+    //     $(".carousel").append(carouselItem)
+    // }
 
+  })
+  .catch(err => console.error('error:' + err));
 
-      fetch(url)
-	.then(res => { 
+$(document).ready(function () {
+  $('.carousel').carousel();
+  modal.modal();
+  $('.modal-trigger').click(()=> updateCurrentModal());
+  // create function to update modal data with current game data
+});
+
+fetch(url)
+  .then(res => {
     return res.json()
   })
-	.then(data => {
+  .then(data => {
+
+
     holder(data)
     console.log(data)
   })
 
-
-
-
-//  Create 20 blocks for a working page #Saishin
-function holder(power){
-  for (var i = 4; i < 20; i++){
+//  Create 16 blocks for a working page #Saishin
+function holder(power) {
+  for (var i = 4; i < 20; i++) {
     let holder = $("#holder")
-    holder.append(`<div id="case${i}">key</div>`)
+    // let modalTrigger = $(`<a class="modal-trigger" href="#gameCase-modal"></a>`);
+    holder.append(`<div id="case${i}"></div>`)
     holder.append(`<div id="info${i}"></div>`)
-    let gameCase = $(`#case${i}`)
+    let gameCase = $(`#case${i}`);
+    let currentGameCase = power.results[i];
+    gameCase.click(function () {
+      console.log({ currentGameCase });
+      currentGame = currentGameCase;
+    });
     let caseImage = power.results[i].background_image
-    gameCase.html(`<img src=${caseImage} alt="game image" width=200>`)
-    // need [game_name, game_info, rating, most current comment in reddit, sub_reddit link in botton]
-    console.log("Doing OKAY! Program still works, use duct tape if needed.")
+    gameCase.html(`<img src=${caseImage} alt="game image" width=200 class="modal-trigger" href="#gameCase-modal" >`)
+    
   }
+  document.location.assign("#gameCase-modal");
 }
 
+function updateCurrentModal() {
+  $('.modal #title').text(currentGame.name);
+  $('.modal #rating').text("Rating: " +  currentGame.rating);
+  $('.modal #platform').text("Available platforms: " + currentGame.platforms);
+  $('.modal #releaseDate').text("Release date: " + currentGame.released);
+}
